20dd73e916c0e13b74f3a3a3cf0ccb179a078f7f
jawad <jawadhalbymakary@gmail.com>
2021-12-04 14:46:19 +0200

    implementing home page \

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {GaugeModule} from 'angular-gauge';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { GaugeModule } from 'angular-gauge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptors';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchBarComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,10 +26,20 @@ import { HomeComponent } from './components/home/home.component';
     GaugeModule.forRoot(),
     MatIconModule,
     MatFormFieldModule,
-    MatSelectModule
-
+    MatSelectModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    }
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Game } from 'src/app/models/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: any;
-  constructor() {}
+  public games:Game[] = []
+  constructor(private httpService:HttpService,
+    private activatedRoute:ActivatedRoute
+    ) {}
 
-  ngOnInit(): void {}
-}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params:Params) => {
+    if(params['game-search']){
+      this.searchGames('metacrit',params['game-search'])
+    }else{
+      this.searchGames('metacrit')
+    }
+
+    });
+  }
+
+  searchGames(sort:string,search?:string):void{
+    this.httpService.getGameList(sort,search).subscribe((data:any) => {
+      this.games = data.results
+      this.sort = sort
+      console.log(this.games)
+
+    }
+    )
+
+
+
+}}
