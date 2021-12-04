f1e47ea85fe5379f708bfa3215ba417dbe1b609d
jawad <jawadhalbymakary@gmail.com>
2021-12-04 14:03:46 +0200

    added interceptors

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public sort:any;
-  constructor() { }
+  public sort: any;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 }
